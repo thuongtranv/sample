2765ec12dc7ecf44ae926f437c26c99db519c4cf
Thuong Tran <thuongtranv@gmail.com>
2017-06-25 22:17:38 +0700

    user 2 change

--- HelloGit/Runner.cs
+++ HelloGit/Runner.cs
@@ -13,6 +13,7 @@ namespace HelloGit
 			Console.Write("Runner 2");
 
 			var calc = new Calculator();
+			Console.WriteLine("User2 make this change.");
 
 			Console.WriteLine(calc.Add(5, 7));
 			Console.WriteLine(calc.Subtract(7,5));
